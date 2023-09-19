66957ce6dcaeba9d8e0604c19c549d5d19553955
Guilherme Ferreira <guiferreiramoraes@outlook.com>
2023-09-18 21:24:32 -0300

    feat: diseases information and queue by preference

--- reg.py
+++ reg.py
@@ -1,12 +1,16 @@
 class Patient:
-    def __init__(self, name, age, is_new, med):
+    def __init__(self, name, age, is_new, med, dis, pref):
         self.name = name
         self.age = age
         self.is_new = is_new
         self.med = med
+        self.dis = dis
+        self.pref = pref
 
     def __str__(self):
-        return f"• {self.name}({self.age} anos, primeira visita: {self.is_new}) \n- Medicamentos em uso: {self.med}"
+        return f"• {self.name}({self.age} anos, primeira visita: {self.is_new}, pref: {self.pref}) \n- Medicamentos em uso: {self.med} \n- Doenças autoimunes: {self.dis}"
+
+## Indice de preferência: 1 = preferencial, 2 = não preferencial
 
 # verifica se os valores(números) inseridos são positivos e inteiros
 def get_integer_input(prompt):
@@ -44,15 +48,29 @@ def get_med_info(prompt):
 
     return med
 
+
+def get_dis_info(prompt):
+    res = get_yes_no_input(prompt)
+    if res:
+        try:
+            dis = str(input("Por favor cite-as abreviadamente: "))
+        except ValueError:
+            print("Por favor insira as doenças a seguir.")
+    else:
+        dis = "Nenhuma"
+
+    return dis
+
 # recebe os valores primários do paciente
 def get_patient_information():
     try:
         name = input("Insira o nome do paciente: ").title()
         age = get_integer_input("Insira a idade do paciente: ")
+        pref = 1 if age <= 10 or age >= 55 else 2
         is_new = get_yes_no_input("É a primeira vez? (s/n): ")
         med = get_med_info("Está tomando algum medicamento? (s/n): ")
-
-        return name, age, "Sim" if is_new else "Não", med
+        dis = get_dis_info("Possui alguma doença autoimune? (s/n): ")
+        return name, age, "Sim" if is_new else "Não", med, dis, pref
 
     except KeyboardInterrupt:
         print("\nOperação interrompida pelo usuário.")
@@ -60,6 +78,6 @@ def get_patient_information():
 
 
 if __name__ == "__main__":
-    name, age, is_new, med = get_patient_information()
-    patient = Patient(name, age, is_new, med)
+    name, age, is_new, med, dis, pref = get_patient_information()
+    patient = Patient(name, age, is_new, med, dis, pref)
     print(patient)
